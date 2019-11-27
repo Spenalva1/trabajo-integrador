b6d185606b37bb09368a25180a894e52be2c3a3a
spenalva1 <penalvasantiagogm@gmail.com>
2019-11-27 16:28:47 -0300

    simulacion de olvide mi contraseña

--- functions.php
+++ functions.php
@@ -54,4 +54,6 @@ function getUserByDni($users, $dni){
     return null;
 }
 
-?>
+function generateRandomString($length = 10) {
+    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+}
