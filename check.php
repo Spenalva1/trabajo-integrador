7eaf469199971778ff4fba848bc87399b6b460d1
spenalva1 <penalvasantiagogm@gmail.com>
2020-02-05 14:30:19 -0300

    laravel vacio para empezar

--- check.php
+++
@@ -1,5 +0,0 @@
-<?php
-date_default_timezone_set("America/Argentina/Buenos_Aires");
-$date = date("Y-m-d H:i:s");
-
-var_dump($date);
--- logOut.php
+++
@@ -1,3 +0,0 @@
-<?php
-include 'clases/Session.php';
-Session::logOut();
