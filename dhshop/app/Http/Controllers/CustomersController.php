2ef4f16395b03d0761ca29a5151ae7e7aee3b7c5
spenalva1 <penalvasantiagogm@gmail.com>
2020-02-08 05:19:21 -0300

    crud hecho (products, marks, categories, customers)

---
+++ dhshop/app/Http/Controllers/CustomersController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Customer;
+
+class CustomersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $Customers = Customer::all();
+        return view('adminCustomers', compact('Customers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Show the confirmation form for destroying the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        $Customer = Customer::find($id);
+        return view('deleteCustomer', compact('Customer')); 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $Customer = Customer::find($id);
+        @unlink(public_path('Customer_img/') . $Customer->image);
+        $Customer->delete();
+        return redirect('/adminCustomers');
+    }
+}
--- dhshop/routes/web.php
+++ dhshop/routes/web.php
@@ -12,5 +12,44 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+
+########### Marks #############
+Route::get('/adminMarks', 'MarksController@index');
+Route::get('/addMark', 'MarksController@create');
+Route::post('/addMark', 'MarksController@store');
+Route::get('/editMark/{id}', 'MarksController@edit');
+Route::post('/editMark/{id}', 'MarksController@update');
+Route::get('/deleteMark/{id}', 'MarksController@delete');
+Route::post('/deleteMark/{id}', 'MarksController@destroy');
+
+########### Categories #############
+Route::get('/adminCategories', 'CategoriesController@index');
+Route::get('/addCategory', 'CategoriesController@create');
+Route::post('/addCategory', 'CategoriesController@store');
+Route::get('/editCategory/{id}', 'CategoriesController@edit');
+Route::post('/editCategory/{id}', 'CategoriesController@update');
+Route::get('/deleteCategory/{id}', 'CategoriesController@delete');
+Route::post('/deleteCategory/{id}', 'CategoriesController@destroy');
+
+########### Prodcuts #############
+Route::get('/adminProducts', 'ProductsController@index');
+Route::get('/addProduct', 'ProductsController@create');
+Route::post('/addProduct', 'ProductsController@store');
+Route::get('/editProduct/{id}', 'ProductsController@edit');
+Route::post('/editProduct/{id}', 'ProductsController@update');
+Route::get('/deleteProduct/{id}', 'ProductsController@delete');
+Route::post('/deleteProduct/{id}', 'ProductsController@destroy');
+
+########### Customers #############
+Route::get('/adminCustomers', 'CustomersController@index');
+Route::get('/deleteCustomer/{id}', 'CustomersController@delete');
+Route::post('/deleteCustomer/{id}', 'CustomersController@destroy');
+
+
