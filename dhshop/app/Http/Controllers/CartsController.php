b9fc577e19f672487cf5925ca3cf3d560465a77a
spenalva1 <penalvasantiagogm@gmail.com>
2020-02-12 23:50:19 -0300

    checkout

--- dhshop/app/Http/Controllers/CartsController.php
+++ dhshop/app/Http/Controllers/CartsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Cart;
 use App\Product;
+use App\Receipt;
+use App\ReceiptProduct;
 use Illuminate\Support\Facades\Auth;
 
 class CartsController extends Controller
@@ -82,6 +84,30 @@ class CartsController extends Controller
             return redirect('/cart')->withErrors($stockErrors); //Avisa que ya no hay stock suficiente
         }
 
+        $Receipt = new Receipt;
+        date_default_timezone_set("America/Argentina/Buenos_Aires");
+        $Receipt->user_id = Auth::user()->id;
+        $Receipt->date = date("Y-m-d H:i:s");
+        $Receipt->save();  // creo el recibo y lo guardo en la base de datos
+
+
+        foreach ($Products as $Product) {
+            $ReceiptProduct = new ReceiptProduct;
+            $ReceiptProduct->receipt_id = $Receipt->id;
+            $ReceiptProduct->product_id = $Product->id;
+            $ReceiptProduct->quantity = $Product->pivot->quantity;
+            $ReceiptProduct->save(); // se registran los productos de la compra
+            $Product->stock -= $Product->pivot->quantity; //se actualiza el stock del producto
+            $Product->save(); //se actualiza el stock del producto
+        }
+
+
         
+        $Cart = Cart::where('user_id','=', Auth::user()->id)->get();
+        foreach ($Cart as $CartItem) {
+            $CartItem->delete(); // Borrar los productos del carrito
+        }
+
+        return redirect('/');
     }
 }
