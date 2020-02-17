5ec10e16c4e1175e4ba0bc0bb9b7684e6c1c2994
spenalva1 <penalvasantiagogm@gmail.com>
2020-02-17 04:14:27 -0300

    correcciones

--- dhshop/__db__/dhshop.sql
+++ dhshop/__db__/dhshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2020 at 06:28 PM
+-- Generation Time: Feb 17, 2020 at 08:10 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -59,8 +59,7 @@ CREATE TABLE `carts` (
 --
 
 INSERT INTO `carts` (`id`, `user_id`, `product_id`, `quantity`) VALUES
-(33, 7, 1, 1),
-(34, 7, 13, 2);
+(37, 7, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -110,8 +109,26 @@ INSERT INTO `marks` (`id`, `name`) VALUES
 (6, 'Sony'),
 (7, 'Fiio'),
 (10, 'Lenovo'),
-(16, 'Kz'),
-(23, 'Corsair');
+(16, 'Kz');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(2, '2020_02_12_202328_column_quantity_added_to_receipts_products', 1);
 
 -- --------------------------------------------------------
 
@@ -146,8 +163,8 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `stock`, `description`, `image`, `mark_id`, `category_id`) VALUES
-(1, 'Xiaomi airdots', 2700, 2, 'pequeños auriculares inalámbricos que cuentan con estuche que también sirve de cargador. Una buena alternativa a los Apple Airpods.', '1.jpg', 2, 3),
-(2, 'Fiio F3', 3600, 99, 'Los FiiO F3 son los nuevos auriculares In-Ear con drivers de Grafeno, un nano material muy reciente con excelentes propiedades para la producción de sonido en alta fidelidad. Los F3 están construidos con la calidad que caracteriza a los productos FiiO y cuentan con un completo set de accesorios que normalmente puede encontrarse en auriculares que duplican y triplican su valor. Los F3 son IEMs que ofrecen excelente calidad de sonido, aislación del exterior y comodidad para una experiencia de sonido de alta definición que puede disfrutarse en todo momento y lugar.', '2.jpg', 7, 4),
+(1, 'Xiaomi airdots', 2700, 10, 'pequeños auriculares inalámbricos que cuentan con estuche que también sirve de cargador. Una buena alternativa a los Apple Airpods.', '1.jpg', 2, 3),
+(2, 'Fiio F3', 3600, 96, 'Los FiiO F3 son los nuevos auriculares In-Ear con drivers de Grafeno, un nano material muy reciente con excelentes propiedades para la producción de sonido en alta fidelidad. Los F3 están construidos con la calidad que caracteriza a los productos FiiO y cuentan con un completo set de accesorios que normalmente puede encontrarse en auriculares que duplican y triplican su valor. Los F3 son IEMs que ofrecen excelente calidad de sonido, aislación del exterior y comodidad para una experiencia de sonido de alta definición que puede disfrutarse en todo momento y lugar.', '2.jpg', 7, 4),
 (3, 'Apple iPhone 11 Pro Dual SIM 256 GB Verde medianoche 4 GB RAM', 142000, 20, '¡El nuevo smartphone que lo tiene todo! Con este lanzamiento, Apple ha superado todos sus récords. Su diseño se destaca por sus líneas finas y distinguidas a partir de una sola hoja de vidrio resistente, combinada con aluminio de excelente calidad.', '3.jpg', 3, 1),
 (4, 'Samsung Buds', 10000, 1, 'En la calle, en el colectivo o en la oficina, tené siempre a mano tus auriculares Samsung y ¡escapate de la rutina por un rato! Vas a poder disfrutar de la música que más te gusta y de tus podcasts favoritos cuando quieras y donde quieras.El formato perfecto para vos. Al ser in-ear, aíslan el ruido del exterior, mejoran la calidad del audio y son de tamaño pequeño para poder insertarse en tu oreja. Son ideales para acompañarte a la hora de hacer ejercicio mientras te sumergís en el mejor sonido envolvente.', '4.jpg', 1, 3),
 (5, 'Xiaomi Redmi Note 8 Dual SIM 64 GB', 20000, 2, 'El sistema operativo avanzado Android 9.0 Pie aprende tus hábitos para adaptarse a tu rutina y lograr la máxima eficiencia de tu equipo. Tu dispositivo tendrá la autonomía necesaria para reducir el consumo energético ajustando el brillo automáticamente y gestionando la batería de manera inteligente para que puedas priorizar el uso de tus aplicaciones habituales.\nCon su pantalla IPS de 6.3\", disfrutá de colores intensos y mayor nitidez en todos tus contenidos. Con su memoria interna de 64 GB siempre tendrás espacio para almacenar archivos y documentos importantes. Además, podrás guardar películas, series y videos para reproducirlos cuando quieras sin conexión. Si necesitas más, podrás agregar una tarjeta micro-SD, que te permitirá contar con 256 GB extras.\n', '5.jpg', 2, 1),
@@ -166,6 +183,14 @@ CREATE TABLE `receipts` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `receipts`
+--
+
+INSERT INTO `receipts` (`id`, `date`, `user_id`) VALUES
+(13, '2020-02-12 17:30:55', 7),
+(14, '2020-02-12 17:35:08', 7);
+
 -- --------------------------------------------------------
 
 --
@@ -175,8 +200,19 @@ CREATE TABLE `receipts` (
 CREATE TABLE `receiptsproducts` (
   `id` int(11) NOT NULL,
   `receipt_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `receiptsproducts`
+--
+
+INSERT INTO `receiptsproducts` (`id`, `receipt_id`, `product_id`, `quantity`) VALUES
+(4, 13, 1, 2),
+(5, 13, 2, 3),
+(6, 14, 1, 2),
+(7, 14, 2, 3);
 
 -- --------------------------------------------------------
 
@@ -203,7 +239,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `birthdate`, `phone`, `dni`, `address`, `image`, `remember_token`) VALUES
-(7, 'Santiago Nicolás', 'Penalva', 'penalvasantiagogm@gmail.com', '$2y$10$1yOIe5CWuhJq8xzinxK94.ylIUfVr0hmyuQEAel4QzIrj6t.n553u', '1999-05-05', '01166400929', '42148622', 'Virrey Cevallos 215, 4to C', '1581203826.jpg', 'at6I910XyDlQeaTzs0enwrMh6HA4QMPuDJgnmHZ9C1iFfxEa1nCMayffrWL0');
+(7, 'Santiago Nicolás', 'Penalva', 'penalvasantiagogm@gmail.com', '$2y$10$MUKQTj1DFsPGXMPM1ARySO6VhbjL/15oXHDEVlCtTgKQdrlepCOh2', '1999-05-05', '01166400929', '42148622', 'Virrey Cevallos 215, 4to C', '1581553513.jpg', 'JH3sOhGw2ZCTGwRfWLWEov65wJaiKSrtrSTg0BVK9fhWTw2bQhud2K3q7Pwe');
 
 --
 -- Indexes for dumped tables
@@ -233,6 +269,12 @@ ALTER TABLE `categories`
 -- Indexes for table `marks`
 --
 ALTER TABLE `marks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -288,7 +330,7 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -301,6 +343,12 @@ ALTER TABLE `categories`
 --
 ALTER TABLE `marks`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `newsletter`
@@ -318,13 +366,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `receipts`
 --
 ALTER TABLE `receipts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `receiptsproducts`
 --
 ALTER TABLE `receiptsproducts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
